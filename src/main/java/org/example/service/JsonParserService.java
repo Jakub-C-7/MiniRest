c731e65009f4b894377fa5766ecfac3d0c5e5ca4
Jakub Chamera <jakubchamera7@gmail.com>
2025-09-07 15:32:46 +0100

    Java Generics for writeObjectToJson

--- src/main/java/org/example/service/JsonParserService.java
+++ src/main/java/org/example/service/JsonParserService.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.extern.slf4j.Slf4j;
-import org.example.entities.CustomerDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Service class for parsing JSON strings to objects and vice versa.
+ */
 @Service
 @Slf4j
 public class JsonParserService {
@@ -18,22 +20,22 @@ public class JsonParserService {
     /**
      * Converts a list of CustomerDetails objects to a JSON string.
      *
-     * @param customerDetailsList list of CustomerDetails objects
+     * @param objectList list of Objects to be converted to JSON
+     * @param <T>        the type of the objects in the list
      * @return JSON string representation of the list
      * @throws JsonProcessingException if serialization fails
      */
-    //todo: use generics too
-    public String writeObjectToJson(List<CustomerDetails> customerDetailsList) throws JsonProcessingException {
-        return mapper.writeValueAsString(customerDetailsList);
+    public <T> String writeObjectToJson(List<T> objectList) throws JsonProcessingException {
+        return mapper.writeValueAsString(objectList);
     }
 
     /**
      * Parses a JSON string representing an array of objects into a list of the desired class type.
      *
      * @param jsonStringArray JSON string representing an array of objects
-     * @param desiredClass an instance of the desired class type
+     * @param desiredClass    an instance of the desired class type
+     * @param <T>             the type of the desired class
      * @return List of objects of the desired class type
-     * @param <T> the type of the desired class
      * @throws JsonProcessingException if deserialization fails
      */
     public <T> List<T> parseJsonStringArray(String jsonStringArray, T desiredClass) throws JsonProcessingException {
